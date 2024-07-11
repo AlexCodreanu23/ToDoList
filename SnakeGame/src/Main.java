6b255242ae9889242f6e9a26c3f982aea63adb33
Alex Codreanu <126508471+AlexCodreanu23@users.noreply.github.com>
2024-07-11 15:59:54 +0300

    Delete SnakeGame directory

--- SnakeGame/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.awt.*;
-
-public class Main {
-    public static void main(String[] args) {
-        new Board();
-        Snake snake = new Snake();
-        snake.moveSnake("left");
-    }
-}
