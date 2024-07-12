deadcefde3e1839b63fecc0e4e72e4f8ef45eba6
Alex Codreanu <126508471+AlexCodreanu23@users.noreply.github.com>
2024-07-12 22:57:43 +0300

    Create App.cs

---
+++ ToDoList-frontend/App.cs
@@ -0,0 +1,92 @@
+body {
+  margin: 0;
+  background-color: #400f8f;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.App {
+  text-align: center;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+
+.App-logo {
+  height: 40vmin;
+  pointer-events: none;
+}
+
+@media (prefers-reduced-motion: no-preference) {
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+  }
+}
+
+.box1 {
+  background-color: #1f0c66;
+  margin: 20px auto; 
+  max-width: 600px; 
+  display: flex;
+  flex-direction: column;
+  border: 5px solid #827a8f;
+  padding: 10px;
+}
+
+.box2 {
+  background-color: #1f0c66;
+  margin: 10px 0;
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+}
+
+textarea {
+  width: 100%;
+  height: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  box-sizing: border-box;
+  margin-bottom: 10px;
+}
+
+.buton1 {
+  background-color: #411ec2;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 5px;
+  color: white;
+  cursor: pointer;
+  margin: 5%;
+}
+
+.App-header {
+  background-color: #400f8f;
+  min-height: 20vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+}
+
+.App-link {
+  color: #61dafb;
+}
+
+@keyframes App-logo-spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
